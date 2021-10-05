f1903a6b435f8d1a0a9c8bf7e6c586d6fa712d50
FabianForsman <fabian.forsman@gmail.com>
2021-10-05 11:48:36 +0200

    Started on week5

--- Exercises/week5/src/exercises/Ex1UseAClass/Rational.py
+++ Exercises/week5/src/exercises/Ex1UseAClass/Rational.py
@@ -9,9 +9,34 @@
 # See:
 # - useaclass/
 # - UseADict
+from math import sqrt
 class Rational:
     # TODO implement class
 
-    def __init__(self):
-        pass
+    def __init__(self, *args):
+        print(args)
+        print(args[0])
+        print(len(args))
+        num = args[0]
+        if len(args) == 1:
+            denom = 1
+        else:
+            denom = args[1]
+        
+        (self.num, self.denom) = self.check_fracture_reduction(num, denom)
 
+    def get_num(self):
+        return self.num
+
+    def get_denom(self):
+        return self.denom 
+
+    def check_fracture_reduction(self, num: int, denom: int):
+        print(num, denom)
+        if denom == None:
+            denom = 1
+        for i in range(2, round(sqrt(num))):
+            if num % i == denom % i:
+                num = num/i
+                denom = denom/i
+        return num, denom
