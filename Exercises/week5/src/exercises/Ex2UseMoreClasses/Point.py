050391d5e7819a58387391d1f9260e21f67a4e06
Fabian Forsman <fabian.forsman@gmail.com>
2021-10-12 18:15:37 +0200

    Completed Exercise 2

--- Exercises/week5/src/exercises/Ex2UseMoreClasses/Point.py
+++ Exercises/week5/src/exercises/Ex2UseMoreClasses/Point.py
@@ -1,22 +1,24 @@
 # package exercises.ex2usemoreclasses
-
 # A class for points in 3D. Should be considered immutable, no mutating methods
 # NOTE: No IO here, this is just the logical concept.
 # To test, run Ex2TestPointTriangle
+from math import *
 class Point():
     # TODO
     def __init__(self,*args):
-        print(args)
         if isinstance(args[0], Point):
             self.x = args[0].x
             self.y = args[0].y
             self.z = args[0].z
         else:
             self.x = args[0]
-            self.y = args[0]
-            self.z = args[0]
+            self.y = args[1]
+            self.z = args[2]
 
     def distance(self, other):
-        d = (self.x - other.x)**2 + (self.y - other.y)**2 + (self.z - other.z)**2
+        dx = float((self.x - other.x))**2
+        dy = float((self.y - other.y))**2
+        dz = float((self.z - other.z))**2
+        d = sqrt(dx + dy + dz)
         print(d)
         return d
--- Exercises/week5/src/exercises/Ex2UseMoreClasses/Triangle.py
+++ Exercises/week5/src/exercises/Ex2UseMoreClasses/Triangle.py
@@ -1,11 +1,24 @@
 # package exercises.ex2usemoreclasses
 
 from Point import Point
-
-
+from math import *
 # A class for a Triangle in 3D - should use Point class and Heron's formula
 # NOTE: No IO here, this is just the logical concept.
 # To test, run Ex2TestPointTriangle
-class Triangle:
+class Triangle(Point):
     # TODO
-    pass
+    def __init__(self, p1: Point, p2: Point, p3: Point):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+
+    def area(self):
+        a: float = self.p1.distance(self.p2)
+        b: float = self.p1.distance(self.p3)
+        c: float = self.p2.distance(self.p3)
+        s: float = (a + b + c) / 2
+        area = sqrt(s * (s - a) * (s - b) * (s - c))
+        return area
+
+    def __eq__(self, other) -> bool:
+        return self.p1 == other.p1 and self.p2 == other.p2 and self.p3 == other.p3
