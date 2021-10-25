4026b6956f08ddf11650b0e2b227045627cd8dc9
Fabian Forsman <fabian.forsman@gmail.com>
2021-10-25 18:12:47 +0200

    Moved course to folder

---
+++ TDA548/Exercises/week3/src/exercises/Ex8AdventureGame.py
@@ -0,0 +1,14 @@
+# package exercises
+
+# For an extra fun challenge: implement a text-based adventure game, in the style of
+# Interactive Fiction (https://en.wikipedia.org/wiki/Interactive_fiction)
+#
+# For this exercise you will need to have understood the following concepts:
+# - Reading and parsing user input (commands)
+# - Defining classes and creating objects (e.g. locations, items, creatures, ...)
+# - Working with structures (e.g. lists)
+# - Updating object state (e.g. when the player affects the world)
+# - ... and more, depending on how you choose to implement the game
+def adventure_game():
+    # TODO this file is your canvas, make it beautiful
+    pass
